ef40cffe46ebcd436c41da1e38a5ebe743673137
Manoel Vitor <manoelvitorka@gmail.com>
2023-03-29 08:40:55 -0700

    Done with Navbar md/lg screens, working on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import { Barbell } from '@phosphor-icons/react'
-import React from 'react'
+import { Barbell, List, X } from '@phosphor-icons/react'
+import React, { useState } from 'react'
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  function handleMenu() {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
-    <header className='flex justify-center border-b-2 border-b-[#a8a8b3] h-32'>
+    <header className='flex justify-center border-b-2 border-b-[#a8a8b3]'>
       <div className='flex justify-between items-center p-4 max-w-[1440px] w-full'>
         <div className='flex text-white text-4xl gap-2 items-center'>
           <Barbell size={40} />
@@ -11,30 +17,47 @@ function Navbar() {
             href="/"
             className='font-logo hover:text-[#a8a8b3] transform ease-in-out duration-700'>TRAINER</a>
         </div>
-        <ul className='flex gap-4 text-[#a8a8b3] items-center'>
-          <li>
-            <a
-              href="/"
-              className='hover:text-white transform ease-in-out duration-300'>Home</a>
-          </li>
-          <li>
-            <a
-              href="/"
-              className='hover:text-white transform ease-in-out duration-300'>About</a>
-          </li>
-          <li>
-            <a
-              href="/"
-              className='hover:text-white transform ease-in-out duration-300'>Services</a>
-          </li>
-          <li
-            className='flex rounded border-[#6833e4] border-2 hover:bg-[#6833e4] transform ease-in-out duration-500 text-white'>
-            <a
-              href="/"
-              className='p-2'>Contact</a>
-          </li>
-        </ul>
+        <nav className='hidden md:flex'>
+          <ul className='flex gap-4 text-[#a8a8b3] items-center'>
+            <li>
+              <a
+                href="/"
+                className='hover:text-white transform ease-in-out duration-300'>Home</a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className='hover:text-white transform ease-in-out duration-300'>About</a>
+            </li>
+            <li>
+              <a
+                href="/"
+                className='hover:text-white transform ease-in-out duration-300'>Services</a>
+            </li>
+            <li
+              className='flex rounded border-[#6833e4] border-2 hover:bg-[#6833e4] transform ease-in-out duration-500 text-white'>
+              <a
+                href="/"
+                className='p-2'>Contact</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className='md:hidden'>
+          {isMenuOpen ?
+            <X
+              size={32}
+              className='text-white cursor-pointer'
+              onClick={handleMenu} /> :
+            <List
+              size={32}
+              className='text-white cursor-pointer'
+              onClick={handleMenu}
+            />}
+        </div>
+
       </div>
+
     </header>
   )
 }
