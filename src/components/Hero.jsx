1e9231adf893fd6266600b5733fed56809a2492a
Manoel Vitor <manoelvitorka@gmail.com>
2023-04-04 08:26:47 -0700

    Working on services section ⌨️

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,8 @@ function Hero() {
       />
       <div className="t-0 l-0 w-full bg-black md:h-[800px] h-[700px] absolute opacity-60 overflow-hidden"></div>
       <div className='flex flex-col justify-center items-center w-full h-full gap-4 md:max-w-2xl z-[1]'>
-        <h1 className='text-white uppercase text-3xl font-bold text-center leading-10'>You don't have to be great to start, but you have to start to be great.</h1>
-        <p className='text-white text-center'>Champions aren't made in the gyms. Champions are made from something they have deep inside them, a desire, a dream, a vision.</p>
+        <h1 className='text-slate-200 uppercase text-3xl font-bold text-center leading-10'>You don't have to be great to start, but you have to start to be great.</h1>
+        <p className='text-slate-200 text-center'>Champions aren't made in the gyms. Champions are made from something they have deep inside them, a desire, a dream, a vision.</p>
         <a href="#" className='rounded-md p-4 text-slate-200  bg-[#6833e4] hover:bg-[#3B2382] shadow-sm cursor-pointer transform ease-in-out duration-500' >START HERE</a>
       </div>
     </div>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Services() {
   return (
-    <div className='text-white'>Services</div>
+    <div className="grid grid-cols-1 md:grid-cols-4  gap-20 h-[300px] justify-items-center m-4">
+      <div className='bg-red-100 w-full '>Service 1</div>
+      <div className='bg-blue-100 w-full'>Service 2</div>
+      <div className='bg-red-100 w-full'>Service 3</div>
+      <div className='bg-blue-100 w-full'>Service 4</div>
+    </div>
   )
 }
 
