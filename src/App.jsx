d22cda45976bca8dc5d3ba45f6eca1ae10796497
Manoel Vitor <manoelvitorka@gmail.com>
2023-04-07 07:57:14 -0700

    Vercel 404 test

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Services from "./components/Services";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
-import NotFound from "./components/pages/NotFound";
+import NoMatch from "./components/pages/NotFound";
 
 function App() {
 
@@ -15,7 +15,7 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="services" element={<Services />} />
         <Route path="contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<NoMatch />} />
       </Route>
     </Routes>
   )
